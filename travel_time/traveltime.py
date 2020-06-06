e5cc8076cd732905308f895cc8c042ed5b8f7362
Bradley Sherman <sirrus233@gmail.com>
2020-06-06 16:17:08 -0700

    Major refactor, using Google Maps API

--- travel_time/traveltime.py
+++ travel_time/traveltime.py
@@ -1,112 +1,126 @@
-import contextlib
-import datetime
-import re
+"""TravelTime is an app to help figure out how long a commute will be, without needing
+to head to a web page or app to check. It will continuously poll live traffic data, and
+display the current distance between work and home.
+"""
+import json
 import time
-import tkinter
-import urllib.parse
-import urllib.request
-import xml.dom.minidom
+import tkinter as tk
+from typing import Any, Dict, Tuple
 
-import bs4
-import selenium.webdriver
+import requests
 
-
-class TravelTime(tkinter.Frame):
-    def __init__(self, master=None):
-        master.title("TravelTime")
-        master.geometry(("%dx%d") % (300, 50))
-        tkinter.Frame.__init__(self, master)
-        self.pack()
-
-        self.message = tkinter.StringVar()
-        self.time = tkinter.StringVar()
-
-        self.labelMessage = tkinter.Label(master, textvariable=self.message)
-        self.labelTime = tkinter.Label(master, textvariable=self.time)
-        self.labelMessage.pack()
-        self.labelTime.pack()
-
-        self.dieFlag = False
-
-        # register callback
-        self.listenID = self.after(1, self.setLabel)
-
-    def getTravelTimeGoogle(self):
-        # Add work and home addresses here
-        work = ""
-        home = ""
-
-        phantomjs = "phantomjs.exe"
-        url = "https://maps.google.com/"
-        with contextlib.closing(selenium.webdriver.PhantomJS(phantomjs)) as driver:
-            driver.implicitly_wait(10)
-            driver.get(url)
-            get_directions = driver.find_element_by_id("d_launch")
-            start = driver.find_element_by_id("d_d")
-            end = driver.find_element_by_id("d_daddr")
-            submit = driver.find_element_by_id("d_sub")
-            get_directions.click()
-            start.send_keys(work)
-            end.send_keys(home)
-            submit.click()
-
-            content = driver.page_source
-            soup = bs4.BeautifulSoup(content)
-            route_tag = soup.find(id="altroute_0")
-
-            color_string = route_tag.find("img")["class"][1]
-            color_regexp = re.compile("dir-traffic-(.*)")
-            color = color_regexp.search(color_string).group(1)
-
-            time_string = route_tag.find(text=re.compile("In current traffic"))
-            time_regexp = re.compile("In current traffic:\s*(.*)")
-            time = time_regexp.search(time_string).group(1)
-
-        return color, time
-
-    def getTravelTimeBing(self):
-        # This is the specific url of your directions request to Bing
-        url = ""
-        # If you're behind a proxy, you need to uncomment this line, and pass an
-        # associative array into the function, of the form {$protocol : $proxy}
-        #
-        # proxy_handler = urllib.request.ProxyHandler()
-        opener = urllib.request.build_opener(proxy_handler)
-        urllib.request.install_opener(opener)
-        data = urllib.request.urlopen(url)
-        xmldoc = xml.dom.minidom.parse(data)
-        nodelist = xmldoc.getElementsByTagName("TravelDurationTraffic")
-        seconds = nodelist.item(0).firstChild.nodeValue
-        minutes = int(int(seconds) / 60)
-        return minutes
-
-    def setLabel(self):
-        if self.dieFlag:
-            time.sleep(300)
-            die()
-
-        # travelTime = self.getTravelTimeBing()
-        (color, travelTime) = self.getTravelTimeGoogle()
-        if color == "green":
-            self.labelTime.config(fg="forest green")
-        elif color == "yellow":
-            self.labelTime.config(fg="goldenrod")
-        elif color == "red":
-            self.labelTime.config(fg="red")
-
-        self.message.set("Time to home: ")
-        self.time.set(str(travelTime))
-
-        hour = datetime.datetime.time(datetime.datetime.now()).hour
-        if hour >= 19 or hour < 8:
-            self.labelMessage.config(fg="red")
-            self.message.set("YOU SHOULD GO HOME")
-            self.time.set("")
-            self.dieFlag = True
-
-        self.listenID = self.after(1000 * 60 * 5, self.setLabel)
+JSON = Dict[str, Any]
 
 
-root = tkinter.Tk()
-TT = TravelTime(root)
-TT.mainloop()
+class TravelTime:
+    """Main application class. Handles the application lifecycle (using Tkinter),
+    querying map data, and processing it for proper display.
+    """
+
+    def __init__(
+        self, root: tk.Tk, width: int, height: int, config: JSON,
+    ):
+        self.root = root
+        self.root.title("TravelTime")
+        self.root.geometry(f"{width}x{height}")
+
+        self.frame = tk.Frame(root)
+        self.frame.pack()
+
+        self.message = tk.StringVar()
+        self.label_message = tk.Label(root, textvariable=self.message)
+        self.label_message.pack()
+
+        self.time = tk.StringVar()
+        self.label_time = tk.Label(root, textvariable=self.time)
+        self.label_time.pack()
+
+        self.config = config
+        self.update_label()
+
+    def update_label(self) -> None:
+        """Update the application display."""
+        if self.is_work_hours():
+            self._update_label_message(0, True)
+        else:
+            expected_time, actual_time = self.travel_time_google()
+            self._update_label_color(expected_time, actual_time)
+            self._update_label_message(actual_time)
+
+        # Schedule the next execution of this method
+        self.root.after(1000 * 60 * 5, self.update_label)
+
+    def _update_label_color(self, expected_time: int, actual_time: int) -> None:
+        """Update the text color, based on how bad the traffic is."""
+        traffic_error = actual_time / expected_time - 1
+        if traffic_error <= 0.2:
+            self.label_time.config(fg="forest green")
+        elif 0.2 < traffic_error <= 0.5:
+            self.label_time.config(fg="goldenrod")
+        elif traffic_error > 0.5:
+            self.label_time.config(fg="red")
+
+    def _update_label_message(self, actual_time: int, work_hours: bool = False) -> None:
+        """Display the current travel time, or a work-life balance message!"""
+        if not work_hours:
+            self.label_message.config(fg="red")
+            self.message.set("YOU SHOULD GO HOME")  # type: ignore
+            self.time.set("")  # type: ignore
+        else:
+            self.message.set("Time to home: ")  # type: ignore
+
+            travel_time = time.gmtime(actual_time)
+            hours = travel_time.tm_hour
+            minutes = travel_time.tm_min
+            if hours > 0:
+                self.time.set(f"{hours} hours {minutes} minutes")  # type: ignore
+            else:
+                self.time.set(f"{minutes} minutes")  # type: ignore
+
+    def travel_time_google(self) -> Tuple[int, int]:
+        """Gets travel time from Google Maps. Requires a Google Cloud Platform API key,
+        which can be provided in the app config file.
+
+        :return: The expected commute duration, and actual duration in traffic, in
+        seconds.
+        """
+        work = self.config["work_address"]
+        home = self.config["home_address"]
+        api_key = self.config["google_api_key"]
+        api_url = "https://www.google.com/maps/api/directions/json"
+        api_params = {
+            "origin": work,
+            "destination": home,
+            "key": api_key,
+            "departure_time": "now",
+        }
+
+        response = requests.get(api_url, api_params)
+        route_data = response.json()["routes"][0]["legs"][0]
+
+        duration = route_data["duration"]["value"]
+        duration_traffic = route_data["duration_in_traffic"]["value"]
+
+        return duration, duration_traffic
+
+    def is_work_hours(self) -> bool:
+        """Check if it is currently reasonable work hours, configured in config file.
+
+        :return: True/False is work hours.
+        """
+        current_time = time.localtime()
+        start_hour: int = self.config["work_start_hour"]
+        end_hour: int = self.config["work_end_hour"]
+        return start_hour < current_time.tm_hour <= end_hour
+
+
+def main() -> None:
+    """Entry-point to launch app."""
+    with open("config/config.json") as config_file:
+        config = json.load(config_file)
+    app = TravelTime(tk.Tk(), 300, 50, config)
+    app.root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
